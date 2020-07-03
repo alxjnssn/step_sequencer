bfe609e68000561fa385019abe5aa0bbfcbe265c
janssea <alex.janssen@uberflip.com>
2020-07-03 14:11:19 -0400

    STEP-100: Change the way steps are interpretted from range input

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const initSequence = () => {
 
 const stepSequence = () => {
   output = [mainSequenceGates[sequenceIndex], mainSequenceFrequencies[sequenceIndex]];
-  console.log(mainSequenceFrequencies[sequenceIndex], sequenceIndex);
+
   if (sequenceIndex === steps - 1)
     sequenceIndex = reset;
   else {
@@ -131,7 +131,7 @@ const armSequenceStepsElement = () => {
 }
 
 const updateSequenceSteps = (event) => {
-  steps = parseInt(event.target.value);
+  steps = parseInt(event.target.value) * 2;
 }
 
 const armModifySequenceElement = () => {
